412ed0cfb49f2a7df0de245a4710936077bd06b3
greenpeace101 <melvin@csp.ph>
2018-01-03 10:48:11 +0800

    1

---
+++ DemoProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+
+        public static String createMessage()
+        {
+            return "Hello World!";
+        }
+    }
+}
